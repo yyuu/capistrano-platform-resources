d4c7b655f92f223d8ad7f511ac93ad8f6f449ba7
Yamashita Yuu <yamashita@geishatokyo.com>
2013-03-25 23:31:09 +0900

    add :platform_identifier and :platform_architecture

--- lib/capistrano/configuration/resources/platform_resources.rb
+++ lib/capistrano/configuration/resources/platform_resources.rb
@@ -8,6 +8,7 @@ module Capistrano
         def self.extended(configuration)
           configuration.load {
             namespace(:platform) {
+              _cset(:platform_identifier) { platform.identifier(fetch(:platform_identifier_options, {})) }
               def identifier(options={})
                 capture((<<-EOS).gsub(/\s+/, " "), options).strip.to_sym
                   if test -f /etc/debian_version; then
@@ -28,20 +29,21 @@ module Capistrano
                 EOS
               end
 
+              _cset(:platform_architecture) { platform.architecture(fetch(:platform_architecture_options, {})) }
               def architecture(options={})
                 arch = capture("uname -m", options).strip.to_sym
                 case arch
-                when /^(i[3-6]86|pentium)$/ then "i386"
-                when /^(amd64|x86_64)$/     then "x86_64"
+                when /^(i[3-6]86|pentium)$/ then :i386
+                when /^(amd64|x86_64)$/     then :x86_64
                 else
-                  arch
+                  arch.to_sym
                 end
               end
 
               namespace(:packages) {
                 def installed?(packages=[], options={})
                   packages = [ packages ].flatten
-                  identifier = ( options.delete(:identifier) || platform.identifier(options) )
+                  identifier = ( options.delete(:identifier) || fetch(:platform_identifier) )
                   if packages.empty?
                     true
                   else
@@ -56,14 +58,13 @@ module Capistrano
 
                 def install(packages=[], options={})
                   packages = [ packages ].flatten
-                  identifier = options.delete(:identifier)
-                  identifier = top.platform.identifier(options) unless identifier
+                  identifier = ( options.delete(:identifier) || fetch(:platform_identifier) )
                   unless packages.empty?
                     case identifier
                     when :debian, :ubuntu
-                      run("#{sudo} apt-get install -q -y #{packages.map { |x| x.dump }.join(" ")}", options)
+                      sudo("apt-get install -q -y #{packages.map { |x| x.dump }.join(" ")}", options)
                     when :centos, :redhat
-                      run("#{sudo} yum install -q -y #{packages.map { |x| x.dump }.join(" ")}", options)
+                      sudo("yum install -q -y #{packages.map { |x| x.dump }.join(" ")}", options)
                     end
                   end
                 end
